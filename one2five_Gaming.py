b91a5e2cfedf692a01c6f63abf958351f14b1569
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2021-10-22 23:31:24 +0800

    version 2.0
    完成了進階規則的程式碼(基礎版)
    進階規則的自動測試尚未完成

--- one2five_Gaming.py
+++ one2five_Gaming.py
@@ -5,9 +5,9 @@
 1. 數字越大者 : 勝
 2. 但 1 勝過 5
 3. 但 2 勝過 4
-"""
-"""
+
 進階規則 : 以基本規則為基底，新增J、Q、K、Joker 四種功能卡
+共三大局，三局內比分高者勝利，若比數相同則加開第四局，若再和局則往下比較小局賽點分數，分數高者勝
 功能卡 : 共三大局，整場遊戲僅能使用一次
     J : 將指定位置的牌 向左交換
     Q : 將指定位置的牌 向右交換
@@ -15,10 +15,11 @@
     Jocker : 雙方玩家的局面 交換(最後發動)
 以基本規則對決一局後，將對決順序排開，審視局面可以使用一張功能卡 (or多張?)擺放在不同位置上
 選定好功能卡，並決定要放在哪個位置發動，雙方玩家攤開功能卡，重新整理盤勢，決定該局勝負，勝者得一點
-比賽共三局，功能卡不得重複使用，最終三局比完，得分高者勝
-若是因為和局導致雙方比數相同，則加開第四局
-加開規則:
-    1. 基礎版:雙方再對決一局，拿剩下的功能卡對決
+比賽共三局，功能卡不得重複使用，最終三局比完，大局得分高者勝
+若是因為和局導致雙方比數相同，則加開第四局，若再和局則往下比較小局得分，得分高者勝
+若第四局再和局那就和局XD，該情況比分為[0, 0, 4]，發生機率非常低
+加開第四局的規則:
+    1. 基礎版:雙方再對決一局，拿剩下的功能卡對決 (已完成)
     2. 瘋狂版:雙方再對決一局
         a.此時四張功能卡都能使用(or四張可同時使用?)，一動決勝負
         b.四張卡都能使用，雙方依序四動，一次可以出一張or不出，每次出完盤整，四動完結算遊戲
@@ -92,6 +93,46 @@ class GAME12345 (object):
             
         return enemy,player
     
+    def BaseRule_Autotest(self,input_list):
+        match = [1, 2, 3, 4, 5] # 對手的牌
+        match_hash = {1:[2,3,4], 2:[3,5], 3:[4,5], 4:[2,5], 5:[1]} 
+        # 雜湊表用於處理勝利條件，若是對手等於1，則勝利條件為玩家出2 or 3 or 4
+        random.shuffle(match) # 電腦的牌隨機洗牌
+        i = 0 # 計次
+        enemy = [] # 對手的牌
+        player = [] # 玩家的牌
+        enemy_deck = [1, 2, 3, 4, 5] # 對手的牌庫
+        player_deck = [1, 2, 3, 4, 5] # 玩家的牌庫
+        enemy_point = 0 # 對手的分數
+        player_point = 0 # 玩家的分數
+        draw = 0 # 和局數
+        
+        while i < 5: # 五局
+            try: #若是輸入的值不符合規則，則會跳出迴圈，並提示輸入正確的格式
+                nums = input_list[i]
+                if nums > 5 or nums < 1: # 限制數字的條件
+                    continue
+                if nums in player: # 限制不能重複出牌
+                    continue
+            except:
+                continue
+            
+            enemy.append(match[i]) # 將對手出過的牌加入清單中
+            player.append(nums) # 將玩家出過的牌加入清單中
+            enemy_deck.remove(match[i]) # 將對手出過的牌移出牌庫
+            player_deck.remove(nums) # 將玩家出過的牌移出牌庫
+            
+            if nums in match_hash[match[i]]:
+                player_point += 1
+            elif nums == match[i]:
+                draw += 1
+            else:
+                enemy_point += 1
+            
+            i += 1
+        # 輸出對手小局得分和玩家小得分
+        return enemy_point,player_point,draw
+    
     def BaseRule_for_Advance(self):
         match = [1, 2, 3, 4, 5] # 對手的牌
         match_hash = {1:[2,3,4], 2:[3,5], 3:[4,5], 4:[2,5], 5:[1]} 
@@ -150,53 +191,14 @@ class GAME12345 (object):
             
         return enemy,player
     
-    def Autoversion(self,input_list):
-        match = [1, 2, 3, 4, 5] # 對手的牌
-        match_hash = {1:[2,3,4], 2:[3,5], 3:[4,5], 4:[2,5], 5:[1]} 
-        # 雜湊表用於處理勝利條件，若是對手等於1，則勝利條件為玩家出2 or 3 or 4
-        random.shuffle(match) # 電腦的牌隨機洗牌
-        i = 0 # 計次
-        enemy = [] # 對手的牌
-        player = [] # 玩家的牌
-        enemy_deck = [1, 2, 3, 4, 5] # 對手的牌庫
-        player_deck = [1, 2, 3, 4, 5] # 玩家的牌庫
-        enemy_point = 0 # 對手的分數
-        player_point = 0 # 玩家的分數
-        draw = 0 # 和局數
-        
-        while i < 5: # 五局
-            try: #若是輸入的值不符合規則，則會跳出迴圈，並提示輸入正確的格式
-                nums = input_list[i]
-                if nums > 5 or nums < 1: # 限制數字的條件
-                    continue
-                if nums in player: # 限制不能重複出牌
-                    continue
-            except:
-                continue
-            
-            enemy.append(match[i]) # 將對手出過的牌加入清單中
-            player.append(nums) # 將玩家出過的牌加入清單中
-            enemy_deck.remove(match[i]) # 將對手出過的牌移出牌庫
-            player_deck.remove(nums) # 將玩家出過的牌移出牌庫
-            
-            if nums in match_hash[match[i]]:
-                player_point += 1
-            elif nums == match[i]:
-                draw += 1
-            else:
-                enemy_point += 1
-            
-            i += 1
-        
-        return enemy_point,player_point
-    
     def who_win(self, enemy, player):
         # 接受最後結果的清單，提供雙方得分以及勝負
         match_hash = {1:[2,3,4], 2:[3,5], 3:[4,5], 4:[2,5], 5:[1]}
-        enemy_point = 0 # 對手的分數
+        enemy_point = 0 # 對手的分數 # 似乎也能考慮小分比數?
         player_point = 0 # 玩家的分數
         draw = 0 # 和局數
         result = [0, 0, 0] # 第一個是對手得分 第二個是玩家得分 第三個是和局
+        Match_point = [0, 0, 0]
         for i in range(5):
             if player[i] in match_hash[enemy[i]]:
                 player_point += 1
@@ -204,32 +206,151 @@ class GAME12345 (object):
                 draw += 1
             else:
                 enemy_point += 1
+        Match_point[0] = enemy_point
+        Match_point[1] = player_point
+        Match_point[2] = draw
         if player_point > enemy_point:
             result[1] = 1
         elif player_point == enemy_point:
             result[2] = 1
         else:
             result[0] = 1
-        return result
+        return result,Match_point # 回傳大局得分和小局得分(清單)
         
-    def AdvanceRule(self):
+    def function_card_JQ(self, deck, card, nums):
+        nums = nums - 1
+        if card == 'J':
+            tmp = deck[nums - 1]
+            deck[nums - 1] = deck[nums]
+            deck[nums] = tmp
+        if card == 'Q':
+            if nums < 4:
+                tmp = deck[nums + 1]
+                deck[nums + 1] = deck[nums]
+                deck[nums] = tmp
+            else:
+                tmp = deck[0]
+                deck[0] = deck[nums]
+                deck[nums] = tmp
+        return deck
+    def function_card_K(self, deck1, deck2, nums):
+        nums = nums - 1
+        tmp = deck2[nums]
+        deck2[nums] = deck1[nums]
+        deck1[nums] = tmp
+        return deck1,deck2
+    def function_card_JOKER(self, deck1, deck2):
+        return deck2,deck1
+    
+    def Advanceloop(self, enemy_deck, player_deck,result,Match_point):
         enemy,player = GAME12345().BaseRule_for_Advance()
+        print('對手的功能牌庫',enemy_deck)
+        print('玩家的功能牌庫',player_deck)
+        again = 0
+        while again < 1:
+            try: #若是輸入的值不符合規則，則會跳出迴圈，並提示輸入正確的格式
+                card = str(input('選擇你的功能卡 : '))
+                if card not in player_deck:
+                    print('請輸入牌庫內的功能卡')
+                    continue
+            except:
+                print('請輸入牌庫內的功能卡') # 若是輸入浮點數之類的會報錯
+            again += 1    
+        while again < 2:
+            try:
+                nums = int(input('選擇功能卡要放置的位置 1-5 : '))
+                if nums > 5 or nums < 1: # 限制數字的條件
+                    print('請輸入1至5區間內的數字')
+                    continue
+            except:
+                print('請輸入指定的數字')
+                continue
+            again += 1
+        tmp_card = random.randrange(len(enemy_deck))
+        enemy_card = enemy_deck[tmp_card]
+        enemy_nums = random.randrange(5) + 1
+        
+        player_deck.remove(card) # 將玩家出過的牌移出牌庫
+        enemy_deck.remove(enemy_card)
+        
+        print('對手出了功能卡:',enemy_card,'放置在',enemy_nums,'位置上')
+        print('玩家出了功能卡:',card,'放置在',nums,'位置上')      
+        
+        if card == 'J' or card == 'Q':
+            player = GAME12345().function_card_JQ(player, card,nums)
+        if enemy_card == 'J' or enemy_card == 'Q':
+            enemy = GAME12345().function_card_JQ(enemy, enemy_card, enemy_nums)
+        if card == 'K':
+            player,enemy = GAME12345().function_card_K(player, enemy, nums)
+        if enemy_card == 'K':
+            player,enemy = GAME12345().function_card_K(player, enemy, enemy_nums)                
+        if card == 'JK':
+            player,enemy = GAME12345().function_card_JOKER(player, enemy)
+        if enemy_card == 'JK':
+            player,enemy = GAME12345().function_card_JOKER(player, enemy)    
+        
+        print('盤面結果')
+        print('對手的盤面', enemy)
+        print('玩家的盤面', player)
+        
+        who_win,point = GAME12345().who_win(enemy, player)
+        if who_win[0] == 1:
+            print('這局你輸了')
+        elif who_win[1] == 1:
+            print('這局你贏了')
+        else:
+            print('和局')
+        result = [who_win[0]+result[0], who_win[1]+result[1], who_win[2]+result[2]] 
+        Match_point = [point[0]+Match_point[0], point[1]+Match_point[1], point[2]+Match_point[2]]
+        print(result,Match_point)
+        return enemy_deck,player_deck,result,Match_point
+            
+    def AdvanceRule(self):
         enemy_deck = ['J', 'Q', 'K', 'JK'] # 對手的功能牌庫
         player_deck = ['J', 'Q', 'K', 'JK'] # 玩家的功能牌庫
-        print('選擇你的功能卡要放置的位置 1-5')
-        
-        
-        
-        
-        
-        
+        result = [0, 0, 0] # 大局記分板 # 第一個是對手得分 第二個是玩家得分 第三個是和局
+        Match_point = [0, 0, 0] #小局記分板
+        for i in range(3): # 三局制
+            enemy_deck,player_deck,result,Match_point = GAME12345().Advanceloop(enemy_deck,player_deck,result,Match_point)
+            if result[0] == 2:
+                print('你輸了QQ')
+                break
+            elif result[1] == 2:
+                print('你獲得了最終勝利')
+                break
+            elif result[2] == 2 and result[0] == 1:
+                print('你輸了QQ')
+                break
+            elif result[2] == 2 and result[1] == 1:
+                print('你獲得了最終勝利')
+                break
+        if result[1] == result[0]:
+            print('和局，準備加開最後一局')
+            enemy_deck,player_deck,result,Match_point = GAME12345().Advanceloop(enemy_deck,player_deck,result,Match_point)
+            if result[0] > result[1]:
+                print('你輸了QQ')
+            elif result[1] > result[0]:
+                print('你獲得了最終勝利')
+            elif Match_point[0] > Match_point[1]:
+                print('你輸了QQ')
+            elif Match_point[1] > Match_point[0]:
+                print('你獲得了最終勝利')
+            else:
+                print('和局')
+        return result,Match_point
+# =============================================================================
+# 測試區塊
+# =============================================================================
+    def AdvanceRule_Autotest(self):
+        print('進階規則自動測試，尚未完成')
+    
         
         
 if __name__ == '__main__':
-    # GAME12345().BaseRule()
-    input_list = [1,2,3,4,5]
-    # GAME12345().Autoversion(input_list)
-    # GAME12345().AdvanceRule()
+    # GAME12345().BaseRule() #基本規則版本
+    
+    # GAME12345().AdvanceRule() #進階規則版本
+    
     enemy = [1,2,3,4,5]
     player = [2,1,4,5,3]
     x = GAME12345().who_win(enemy, player)
--- testfile.py
+++ testfile.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 from one2five_Gaming import *
 
+# 功能區塊
+# GAME12345().BaseRule() #基本規則版本
+    
+# GAME12345().AdvanceRule() #進階規則版本
+
+# 測試區塊1 : 模擬基本規則的對決
 enemy_point_sum = 0
 player_point_sum = 0
-for i in range(100): # 模擬對決
+draw_sum = 0
+for i in range(100): 
     input_list = [1,2,3,4,5]
     random.shuffle(input_list)
-    ep,pp = GAME12345().Autoversion(input_list)
-    
-    enemy_point_sum = enemy_point_sum + ep
+    ep,pp,d = GAME12345().BaseRule_Autotest(input_list)
+    # 一局基本規則下，ep = 對方所得點數，pp = 玩家所得點數
+    enemy_point_sum = enemy_point_sum + ep #統計多局下雙方得點的總和
     player_point_sum = player_point_sum + pp
+    draw_sum = draw_sum + d
+print(enemy_point_sum,player_point_sum,draw_sum)
+# 測試結果模擬基本規則下對局一百萬次，結果為[2001117,2001204,997679]
+# 對手勝利與玩家勝利比數接近，比例約為2:2:1
+# 隨機對決的測試結果為具有公平性
 
-# print(enemy_point_sum,player_point_sum)
-
-x = [1,0,0]
-y = [1,1,0]
-y = [x[0]+y[0],x[1]+y[1],x[2]+y[2]]
-print(y)
+# 測試區塊2 : 模擬進階規則的對決
