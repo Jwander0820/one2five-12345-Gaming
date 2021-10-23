320ab785807b0c3f6b51706656095a8e1e4f36d2
Jwander0820 <66709644+Jwander0820@users.noreply.github.com>
2021-10-23 23:10:00 +0800

    version 2.2
    1. 完成進階規則的自動測試程式碼，包含勝敗測試與九種案例分析
    2. 加入程式碼註解
    3. 優化基本規則自動測試的程式碼，v2.0測試一百萬次耗時11.86秒，v2.2測試9.96秒，並同時能記錄大局勝負和小局得分
    4. 完成了進階規則的勝敗案例分析、與九種狀況案例分析

--- testfile.py
+++ testfile.py
@@ -1,26 +1,110 @@
 # -*- coding: utf-8 -*-
 from one2five_Gaming import *
+import time
 
+# =============================================================================
 # 功能區塊
+# =============================================================================
+
 # GAME12345().BaseRule() #基本規則版本
-    
 # GAME12345().AdvanceRule() #進階規則版本
 
-# 測試區塊1 : 模擬基本規則的對決
-enemy_point_sum = 0
-player_point_sum = 0
-draw_sum = 0
-for i in range(100): 
-    input_list = [1,2,3,4,5]
-    random.shuffle(input_list)
-    ep,pp,d = GAME12345().BaseRule_Autotest(input_list)
-    # 一局基本規則下，ep = 對方所得點數，pp = 玩家所得點數
-    enemy_point_sum = enemy_point_sum + ep #統計多局下雙方得點的總和
-    player_point_sum = player_point_sum + pp
-    draw_sum = draw_sum + d
-print(enemy_point_sum,player_point_sum,draw_sum)
-# 測試結果模擬基本規則下對局一百萬次，結果為[2001117,2001204,997679]
-# 對手勝利與玩家勝利比數接近，比例約為2:2:1
-# 隨機對決的測試結果為具有公平性
+# =============================================================================
+# 測試區塊
+# =============================================================================
+# 三種測試案例
+# 測試案例1 : 模擬基本規則的對決 - 勝敗情況簡易分析
+# 測試案例2 : 模擬進階規則的對決 - 勝敗情況簡易分析
+# 測試案例3 : 模擬進階規則的對決 - 勝敗案例分析
 
-# 測試區塊2 : 模擬進階規則的對決
+test_case = 1
+
+# 測試案例1 : 模擬基本規則的對決 - 勝敗情況簡易分析
+if test_case == 1:
+    # 百萬次測試時間約10秒
+    result = [0, 0, 0] # 大局得分(在基本測試中則代表勝利的次數)
+    Match_point = [0, 0, 0] # 小局得分
+    # 順序 => [對手勝利次數, 玩家勝利次數. 和局數]
+    time_start = time.time()
+    
+    for i in range(100000): 
+        input_list = [1,2,3,4,5]
+        random.shuffle(input_list) # 將輸入隨機洗牌
+        oneset,setpoint = GAME12345().BaseRule_Autotest(input_list)
+        # oneset為一輪的勝負(大局得分)，setpoint為5小局得分
+        result = [oneset[0]+result[0], oneset[1]+result[1], oneset[2]+result[2]] 
+        Match_point = [setpoint[0]+Match_point[0], setpoint[1]+Match_point[1], setpoint[2]+Match_point[2]]
+    print(result,Match_point)
+    
+    time_end = time.time()
+    time_c = time_end - time_start
+    print('time cost : ', time_c)
+    
+    # 測試結果模擬基本規則下對局一千萬次，結果為小局得分總和比[19999895, 19997129, 10002976]
+    # 對手勝利與玩家勝利比數[3667487, 3666824, 2665689]
+    # 勝率約為36.67% (敗率相同)；和局機率為26.65%
+    # 證明基本規則的遊戲方式具有公平性
+    
+    
+# 測試案例2 : 模擬進階規則的對決 - 勝敗情況簡易分析
+if test_case == 2:
+    # 百萬次測試時間約49秒
+    result = [0, 0, 0] # 大局得分
+    Match_point = [0, 0, 0] # 小局得分
+    case = [0, 0, 0]# [對手勝利次數:玩家勝利次數:和局數]
+    time_start = time.time()
+    
+    for i in range(100000): 
+        oneset,setpoint,who_win = GAME12345().AdvanceRule_Autotest()
+        # oneset為大局得分，setpoint為小局得分總和，who_win為一輪的勝負[對方勝,玩家勝,和局]
+        result = [oneset[0]+result[0], oneset[1]+result[1], oneset[2]+result[2]] 
+        Match_point = [setpoint[0]+Match_point[0], setpoint[1]+Match_point[1], setpoint[2]+Match_point[2]]
+        case = [who_win[0]+case[0], who_win[1]+case[1], who_win[2]+case[2]]
+    print(result,Match_point,case)
+    
+    time_end = time.time()
+    time_c = time_end - time_start
+    print('time cost : ', time_c)
+    # 測試結果模擬基本規則下對局一千萬次，結果為小局得分總和比[11237776, 11232699, 6760162]
+    # 對手勝利與玩家勝利比數為[4915482, 4912730, 171788]
+    # 勝率約為49.14% (拜率相同)；和局機率為1.72%
+    # 證明進階規則的遊戲方式具有公平性 (一億次的結果類似，和局率為1.72%)
+
+
+# 測試案例3 : 模擬進階規則的對決 - 勝敗案例分析
+# 相同案例會合併作計算(因為機率相同)，如[2,0,0]和[0,2,0]算做同一案例，共九種案例
+# 勝利 = WIN = W；敗北 = LOSE = L；和局 = DRAW = D；勝敗都以單一視角考慮
+# [直落二: 二勝一和: 二勝一敗: 一勝二和: 二勝一敗一和: 一勝三和: 小分勝: 小分和: 四和]
+# [2W : 2W1D : 2W1L : 1W2D : 2W1L1D : 1W3D : 1W1L2D小分W : 1W1L2D小分D : 4D]
+if test_case == 3:
+    result = [0, 0, 0] # 大局得分
+    Match_point = [0, 0, 0] # 小局得分
+    case = [0, 0, 0, 0, 0, 0, 0, 0, 0] # 九種案例對應其發生次數
+    # [2W : 2W1D : 2W1L : 1W2D : 2W1L1D : 1W3D : 1W1L2D小分W : 1W1L2D小分D : 4D]
+    time_start = time.time()
+    
+    for i in range(100000): 
+        oneset,setpoint,who_win = GAME12345().AdvanceRule_Autotest_Case_Analysis()
+
+        result = [oneset[0]+result[0], oneset[1]+result[1], oneset[2]+result[2]] 
+        Match_point = [setpoint[0]+Match_point[0], setpoint[1]+Match_point[1], setpoint[2]+Match_point[2]]
+        case = [who_win[0]+case[0], who_win[1]+case[1], who_win[2]+case[2]
+                  , who_win[3]+case[3], who_win[4]+case[4], who_win[5]+case[5]
+                  , who_win[6]+case[6], who_win[7]+case[7], who_win[8]+case[8]]
+    print(result,Match_point,case)
+    
+    time_end = time.time()
+    time_c = time_end - time_start
+    print('time cost : ', time_c)
+    
+    # 模擬進階規則對決一億次，case為九種案例的次數
+    # [29520439, 13720297, 22657791, 12307907, 15831291, 939762, 3301985, 1445275, 275253]
+    # [29.52, 13.72, 22.66, 12.31, 15.83, 0.94, 3.30, 1.45, 0.27] # 百分率
+
+
+
+
+
+
+
+
